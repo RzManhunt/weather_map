2952548fc66989097b0a375819f9c3394bacf2c7
Luis <lamsarmiento@gmail.com>
2019-11-30 17:26:56 -0400

    Updates

---
+++ precache-manifest.48ce14ad0eea0e37d9f821ab6e628508.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca958fdd3e064e4a6f5857355e52c09",
+    "url": "/weather_map/index.html"
+  },
+  {
+    "revision": "575db1266649e2b95ade",
+    "url": "/weather_map/static/css/main.583976c8.chunk.css"
+  },
+  {
+    "revision": "68a4ca5baa51a1d790a7",
+    "url": "/weather_map/static/js/2.6f4f2fc0.chunk.js"
+  },
+  {
+    "revision": "575db1266649e2b95ade",
+    "url": "/weather_map/static/js/main.31744e33.chunk.js"
+  },
+  {
+    "revision": "df8ef0c12f0b921f4651",
+    "url": "/weather_map/static/js/runtime-main.dbc74be1.js"
+  }
+]);
